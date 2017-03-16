39ef5cf8591566dae7720610e0d9064e49fe0bc2
Cody Stoltman <particlebanana@gmail.com>
2017-03-16 17:58:06 -0500

    start working through the example

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component.js';
+import { HeroDetailComponent } from './hero-detail.component.js';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
